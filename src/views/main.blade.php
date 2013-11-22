457c112a4636cf46c66fe23192a12bd8ac34364b
Stef Horner <shorner@wearearchitect.com>
2013-11-22 08:41:06 +0000

    namespace subview

--- src/views/main.blade.php
+++ src/views/main.blade.php
@@ -2,6 +2,6 @@
 {{-- @todo use $menu properties for ul element --}}
 <ul>
 	@foreach( $menu->getItems() as $item )
-		{{ View::make('menu.item')->with( 'item', $item ) }}
+		{{ View::make('menu::item')->with( 'item', $item ) }}
 	@endforeach
 </ul>
